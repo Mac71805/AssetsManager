676bbda7b909eafee3425138c422576f241f6f92
Mac71805 <miguel71805@gmail.com>
2026-05-12 14:12:52 -0400

    Finish the constructor and getter/setters for the vehicle and house class

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -1,4 +1,38 @@
 package org.example;
 
-public class Vehicle {
+public class Vehicle extends Asset{
+    private int year;
+    private String makeModel;
+    private int odometer;
+
+    public Vehicle(String description, String dataAcquired, double originalCost) {
+        super(description, dataAcquired, originalCost);
+        this.year = year;
+        this.makeModel = makeModel;
+        this.odometer = odometer;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
 }
